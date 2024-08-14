6fbec48c6a2eb78390b4b41bc8aa096249ffbe4f
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-14 19:34:07 -0300

    first part of data entry

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,5 +1,25 @@
 const {Service:ServiceModel} = require("../models/Services");
 
-const serviceController = {};
+const serviceController = {
+
+    create: async(req,res) =>{
+        try {
+            const service = {
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image
+            }
+
+            const response = await ServiceModel.create(service); // the "create" function comes from mongoose
+
+            res.status(201).json({response, msg: "serviço criado com sucesso!"})
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+};
 
 module.exports = serviceController;
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -2,6 +2,6 @@ const router = require("express").Router()
 
 const serviceController = require("../controllers/serviceController");
 
-//funções
+router.route("/services").post((req, res) => serviceController.create(req,res));
 
 module.exports = router;
