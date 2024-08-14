81ed0850568fdd37abbcc63ea0deac3b21321e1c
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-14 19:52:00 -0300

    .

--- backend/models/Services.js
+++ backend/models/Services.js
@@ -5,19 +5,15 @@ const {Schema} = mongoose;
 const serviceSchema = new Schema({ //model skeleton
     name:{
         type:String,
-        required: true
     },
     description:{
         type:String,
-        required: true
     },
     price:{
         type:Number,
-        required: true
     },
     image:{
         type:String, // url image
-        required: true
     },
 },
 {timestamps:true}  // timestamps is used to save the register creation date and updated date
